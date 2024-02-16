4d4b930cea31054a014d323346b3b835b049fb98
yuri_lopukhin <fol2203012@yandex.ru>
2024-02-16 15:50:30 +0300

    feat: works well. Version witout animation

--- src/components/AccordionItem.tsx
+++ src/components/AccordionItem.tsx
@@ -1,43 +1,26 @@
-import {
-  useEffect,
-  useRef,
-  useState
-} from 'react';
-import { getRefValue } from '../lib/hooks';
 import { AccordionData } from '../types';
 import './AccordionItem.css';
 
-function AccordionItem({
-  data,
-  isOpen,
-  btnOnClick,
-  idx,
-}: {
-  data: AccordionData;
-  isOpen: boolean;
-  btnOnClick: () => void;
-  idx:number;
-}) {
-  const contentRef = useRef<HTMLDivElement>(null);
-  const [height, setHeight] = useState(0);
-
-  useEffect(() => {
-    if (isOpen) {
-      const contentEl = getRefValue(contentRef);
-
-      setHeight(contentEl.scrollHeight);
-    } else {
-      setHeight(0);
-    }
-  }, [isOpen]);
+function AccordionItem(
+  {
+    data,
+    isOpen,
+    btnOnClick,
+    idx,
+  }: {
+    data: AccordionData;
+    isOpen: boolean;
+    btnOnClick: () => void;
+    idx: number;
+  }) {
 
   return (
     <li className={`accordion-item ${isOpen ? 'active' : ''}`}>
-      <button id={`accordion-button-${idx}`} aria-expanded={isOpen} onClick={btnOnClick}>
+      <button id={`accordion-button-${idx}`} onClick={btnOnClick}>
         <span className="accordion-title">{data.title}</span>
       </button>
       <div className="accordion-item-container">
-        <div ref={contentRef} className="accordion-item-content" style={{height}}>
+        <div className="accordion-item-content">
           {data.content}
         </div>
       </div>
